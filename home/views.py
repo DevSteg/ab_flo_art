4c3a1af3f1e13a11431994a3da7008aafa807fd4
James Stephens <j.l.stephens91@gmail.com>
2021-05-01 14:12:47 +0000

    Add and link About template

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ Render the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ Render the about page """
+    return render(request, 'home/about.html')
