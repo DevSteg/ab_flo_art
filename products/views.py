1564337264a74caaf964767906e725e1040c98ae
James Stephens <j.l.stephens91@gmail.com>
2021-05-07 21:16:21 +0000

    Add filtering functionality and links to products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -7,9 +7,17 @@ from .models import Product
 def all_products(request):
     """ Renders all products """
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
-        "products": products
+        'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
