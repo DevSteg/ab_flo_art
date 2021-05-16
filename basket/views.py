32570cbaf523c2f5898ef030d86cc806e0977a16
James Stephens <j.l.stephens91@gmail.com>
2021-05-16 14:01:39 +0000

    Add and initialize bs-toasts

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse, reverse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -11,14 +13,17 @@ def basket(request):
 def add_to_basket(request, item_id):
     """ View to add a specified quantity of a product to the bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f"Added {product.name} to your basket!")
     else:
         basket[item_id] = quantity
+        messages.success(request, f"Added {product.name} to your basket!")
 
     request.session['basket'] = basket
 
