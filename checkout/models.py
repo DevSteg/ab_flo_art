5fb7385214e44151919d2ab4e051661655da116e
James Stephens <j.l.stephens91@gmail.com>
2021-05-16 16:33:05 +0000

    Add OrderLineItem model to checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,8 @@
+import uuid
+from django.db.models import Sum
+from django.conf import settings
 from django.db import models
+from products.models import Product
 
 # Create your models here.
 
@@ -22,3 +26,15 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(
+        max_digits=8, decimal_places=2,
+        null=False, blank=False, editable=False)
