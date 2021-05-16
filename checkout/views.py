322fa65314fba442652496a258952da77dcaea21
James Stephens <j.l.stephens91@gmail.com>
2021-05-16 20:57:42 +0000

    Add checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 # Create your views here.
+
+
+def checkout(request):
+    """ View to render the shopping basket page """
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, 'There are no items in your basket')
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, 'checkout/checkout.html', context)
