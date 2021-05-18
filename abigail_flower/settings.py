a2f975322eeed3bfdc0b86927a408c61e0af6e48
James Stephens <j.l.stephens91@gmail.com>
2021-05-18 19:39:19 +0000

    Add checkout form and install crispy_bootstrap5

--- abigail_flower/settings.py
+++ abigail_flower/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'basket',
     'checkout',
     'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'abigail_flower.urls'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 TEMPLATES = [
     {
