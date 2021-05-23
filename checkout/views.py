25bb426e7eafe92078c0e215611c6d45d341e2ab
James Stephens <j.l.stephens91@gmail.com>
2021-05-23 21:21:40 +0000

    Add stripe paymentIntent functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from basket.context import basket_content
+import stripe
 
 # Create your views here.
 
 
 def checkout(request):
-    """ View to render the shopping basket page """
+    """
+    View to render the shopping basket page
+    and handle the stripe payment
+    """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, 'There are no items in your basket')
         return redirect(reverse('products'))
 
+    current_basket = basket_content(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            'Stripe public key is missing. Did you forget to set it?')
+
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IsaNNDTPFyBfatlFYO2kI6MnoUNk2tCFuJ4uC7JjdB4C07Pj7n6kUyr3jm8iO7Xq7pRdliJ6pcVg7saadcH1srV00QIRb9eko',
-        'client_secret': 'test_client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
