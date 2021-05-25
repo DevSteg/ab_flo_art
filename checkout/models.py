3fb8bda62a177be75bd4c71448a66a25902cad42
James Stephens <j.l.stephens91@gmail.com>
2021-05-25 20:19:21 +0000

    Create UserProfile model, add field to order model

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.db import models
 from products.models import Product
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 # Create your models here.
 
@@ -11,6 +12,9 @@ from django_countries.fields import CountryField
 class Order(models.Model):
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=60, null=False, blank=False)
     email = models.EmailField(max_length=300, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    """
+    User Profile modle to handle maintaining delivery address
+    and order history.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_city = models.CharField(max_length=50, null=True, blank=True)
+    default_county = models.CharField(max_length=50, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_update_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the users profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing user just save profile
+    instance.userprofile.save()
