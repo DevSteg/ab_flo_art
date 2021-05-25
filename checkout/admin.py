d63c8b20fef2abca4edcd7fc49b594f73a6f8346
James Stephens <j.l.stephens91@gmail.com>
2021-05-25 14:10:50 +0000

    Add fields to Order model and migrate

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,12 +14,13 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInLine, )
 
     readonly_fields = ('order_number', 'date',
-                       'delivery_cost', 'order_total', 'grand_total',)
+                       'delivery_cost', 'order_total', 'grand_total',
+                       'original_basket', 'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
               'street_address1', 'street_address2', 'postcode', 'town_city',
-              'county', 'country', 'delivery_cost',
-              'order_total', 'grand_total')
+              'county', 'country', 'delivery_cost', 'order_total',
+              'grand_total', 'original_basket', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost', 'grand_total')
