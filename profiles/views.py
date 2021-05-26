9935f73f3066f7e2c16099633309af4bc75dfe8d
James Stephens <j.l.stephens91@gmail.com>
2021-05-26 20:40:15 +0000

    Add profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from .models import UserProfile
+from django.contrib import messages
+from .forms import ProfileForm
 
 # Create your views here.
+
+
+def profile(request):
+    """ View to display the users profile"""
+    user = request.user
+    if user.is_authenticated:
+        profile = get_object_or_404(UserProfile, user=user)
+
+        form = ProfileForm(instance=profile)
+        orders = profile.orders.all()
+
+        context = {
+            'form': form,
+            'orders': orders,
+        }
+
+        return render(request, 'profiles/profile.html', context)
+    else:
+        messages.error(request, 'Please login first!')
+        return redirect(reverse('account_login'))
