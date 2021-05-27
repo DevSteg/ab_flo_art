00dc9a41536cd96fa1f9328d204ecc3a9cbe81a9
James Stephens <j.l.stephens91@gmail.com>
2021-05-27 22:37:16 +0000

    Remove Secret key and set debug

--- abigail_flower/settings.py
+++ abigail_flower/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qcg#wl-w2v4%ibkm1a(4wswqi6(ud9jqy(n&i!3z5#3_4h*@#2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['abigail-flower.herokuapp.com', 'localhost']
 
