67d25b3b124ec4f10fbbf6b4cfabf221d7315764
James Stephens <j.l.stephens91@gmail.com>
2021-05-27 16:39:51 +0000

    Add edit_product view with form post logic

--- products/views.py
+++ products/views.py
@@ -62,3 +62,32 @@ def add_product(request):
     else:
         messages.error(request, 'You must be admin to access this page')
         return render(request, 'home/index.html')
+
+
+def edit_product(request, product_id):
+    """ Edit and existing product in the store """
+    if request.user.is_superuser:
+        product = get_object_or_404(Product, pk=product_id)
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES, instance=product)
+            if form.is_valid():
+                form.save()
+                messages.success(
+                    request, f'{product.name} Updated Successfully')
+                return redirect(reverse('product_detail', args=[product.id]))
+            else:
+                messages.error(
+                    request, 'Failed to update product, Please check the form is correct')
+        else:
+            form = ProductForm(instance=product)
+            messages.info(request, f'You are editing {product.name}')
+
+        context = {
+            'form': form,
+            'product': product,
+        }
+
+        return render(request, 'products/edit_product.html', context)
+    else:
+        messages.error(request, 'You must be admin to access this page')
+        return render(request, 'home/index.html')
