434ec0ce108b7ae81efedd360de02e4d705ed0c0
James Stephens <j.l.stephens91@gmail.com>
2021-05-27 13:38:48 +0000

    Add add_product view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -32,3 +34,31 @@ def product_info(request, product_id):
     }
 
     return render(request, 'products/product_info.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    # Only accessible to superusers
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'New product added successfully!')
+                return redirect(reverse('add_product'))
+            else:
+                messages.error(
+                    request, 'Failed to add product, Please check the form is correct')
+        else:
+            form = ProductForm()
+
+        form = ProductForm()
+
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'products/add_product.html', context)
+    else:
+        messages.error(request, 'You must be admin to access this page')
+        return render(request, 'home/index.html')
